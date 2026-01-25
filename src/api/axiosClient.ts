cdbf463dfe80c565cda90532c1fba6248a3420db
Pozhyla <mrbaymiks@gmail.com>
2026-01-25 12:08:23 +0100

    Add Axios configuration

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const $api = axios.create({
-  baseURL: "", // Proxy бере url з vite.config
+  baseURL: import.meta.env.VITE_API_URL || "",
   headers: { "Content-Type": "application/json" },
 });
 
